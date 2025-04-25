904dd238432408babfea67c468075944e8b517cd
Programacion79 <programacionweb1979>
2025-04-24 21:00:44 -0500

    Creamos la capa del controlador y relizamos el codigo

--- src/main/java/com/cesde/biblioteca/Controlador/ControladorUsuario.java
+++ src/main/java/com/cesde/biblioteca/Controlador/ControladorUsuario.java
@@ -1,2 +1,30 @@
-package com.cesde.biblioteca.Controlador;public class ControladorUsuario {
+package com.cesde.biblioteca.Controlador;
+
+
+import com.cesde.biblioteca.Modelo.Usuario;
+import com.cesde.biblioteca.Servicios.Usuarioservicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuarios")
+public class ControladorUsuario {
+    //Inyectar los servicios
+    @Autowired
+    Usuarioservicio usuarioservicio;
+
+    public ResponseEntity<?>gurdarUsuario(@ResponseBody Usuario datosnuevos)throws Exception{
+        try {
+            ResponseEntity
+                    .status()
+                    .body();
+        }catch (Exception error){
+            ResponseEntity
+                    .status()
+                    .body();
+        }
+    }
 }
--- src/main/java/com/cesde/biblioteca/Servicios/BibliotecaApplication.java
+++ src/main/java/com/cesde/biblioteca/Servicios/BibliotecaApplication.java
@@ -1,4 +1,4 @@
-package com.cesde.biblioteca;
+package com.cesde.biblioteca.Servicios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
